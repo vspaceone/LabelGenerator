d17f58710eeb97998d0aa70e9aecc21645a9caee
noppelmax <max@noppelmax.online>
2021-05-10 20:54:14 +0200

    PEP compatible

--- test/testFlask.py
+++ test/testFlask.py
@@ -5,22 +5,19 @@
 import unittest
 
 import flask_unittest
-import flask.globals
-import urllib.parse
-
 
 from src.main.Server import app
 
-class TestFoo(flask_unittest.ClientTestCase):
+
+class TestFlask(flask_unittest.ClientTestCase):
     app = app
-    labels = ["give_away","public","owner_only","instructed","documented"]
+    labels = ["give_away", "public", "owner_only", "instructed", "documented"]
 
     def setUp(self, client):
         pass
 
     def tearDown(self, client):
         pass
-
 
     def test_invalidPath(self, client):
         status = client.get('/invalidPath').status_code
@@ -43,13 +40,13 @@ class TestFoo(flask_unittest.ClientTestCase):
 
     def test_png_CheckAllLabels(self, client):
 
-        for l in self.labels:
-            status = client.get(f"/{l}/test.png").status_code
+        for label in self.labels:
+            status = client.get(f"/{label}/test.png").status_code
             assert(status == 200)
 
     def test_png_AllCharacters(self, client):
-        for i in range(0,0xff):
-            if i != 0x2f: # for all characters except '/'
+        for i in range(0, 0xff):
+            if i != 0x2f:  # for all characters except '/'
                 status = client.get(f"/give_away/%"+'{:0>2x}'.format(i)+"test.png").status_code
                 assert(status == 200)
             else:
@@ -68,13 +65,13 @@ class TestFoo(flask_unittest.ClientTestCase):
         assert(status == 200)
 
     def test_jpeg_CheckAllLabels(self, client):
-        for l in self.labels:
-            status = client.get(f"/{l}/test.jpeg").status_code
+        for label in self.labels:
+            status = client.get(f"/{label}/test.jpeg").status_code
             assert(status == 200)
 
     def test_jpeg_AllCharacters(self, client):
-        for i in range(0,0xff):
-            if i != 0x2f: # for all characters except '/'
+        for i in range(0, 0xff):
+            if i != 0x2f:  # for all characters except '/'
                 status = client.get(f"/give_away/%"+'{:0>2x}'.format(i)+"test.jpeg").status_code
                 assert(status == 200)
             else:
@@ -84,6 +81,7 @@ class TestFoo(flask_unittest.ClientTestCase):
     def test_home(self, client):
         status = client.get('/').status_code
         assert(status == 200)
+
 
 if __name__ == '__main__':
     unittest.main()
